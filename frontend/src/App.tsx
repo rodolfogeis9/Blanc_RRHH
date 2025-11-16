6801739726deace81a43405362c290b32912e9c9
rodolfogeis9 <rodolfo.geis@gmail.com>
2025-11-16 15:51:45 -0300

    Add protected dashboards for employee and admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,17 @@ import { Box } from "@chakra-ui/react";
 import LoginPage from "./pages/auth/LoginPage";
 import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
 import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
+import EmployeeDashboard from "./pages/employee/EmployeeDashboard";
 import EmployeeProfilePage from "./pages/employee/EmployeeProfilePage";
+import EmployeeDocumentsPage from "./pages/employee/EmployeeDocumentsPage";
 import EmployeeVacationsPage from "./pages/employee/EmployeeVacationsPage";
+import EmployeeLicensesPage from "./pages/employee/EmployeeLicensesPage";
+
+import AdminEmployeesPage from "./pages/admin/AdminEmployeesPage";
+import AdminVacationsPage from "./pages/admin/AdminVacationsPage";
+import AdminAuditPage from "./pages/admin/AdminAuditPage";
 
 function App() {
   return (
@@ -21,9 +29,21 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
 
-        {/* Empleado (cuando ya estés logueado) */}
-        <Route path="/mi-perfil" element={<EmployeeProfilePage />} />
-        <Route path="/mis-vacaciones" element={<EmployeeVacationsPage />} />
+        {/* Portal empleado */}
+        <Route element={<ProtectedRoute roles={['EMPLEADO']} />}>
+          <Route path="/portal" element={<EmployeeDashboard />} />
+          <Route path="/portal/mis-datos" element={<EmployeeProfilePage />} />
+          <Route path="/portal/documentos" element={<EmployeeDocumentsPage />} />
+          <Route path="/portal/vacaciones" element={<EmployeeVacationsPage />} />
+          <Route path="/portal/licencias" element={<EmployeeLicensesPage />} />
+        </Route>
+
+        {/* Portal administración */}
+        <Route element={<ProtectedRoute roles={['ADMIN_RRHH', 'ADMIN_DIRECCION']} />}>
+          <Route path="/admin/empleados" element={<AdminEmployeesPage />} />
+          <Route path="/admin/solicitudes" element={<AdminVacationsPage />} />
+          <Route path="/admin/auditoria" element={<AdminAuditPage />} />
+        </Route>
 
         {/* 404 simple */}
         <Route path="*" element={<Box p={10}>Página no encontrada</Box>} />
